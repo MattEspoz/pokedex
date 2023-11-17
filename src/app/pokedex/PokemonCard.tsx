517128cccef680571f66414fa78224f37207d399
Matt Espinoza <70907264+MattEspoz@users.noreply.github.com>
2023-11-17 11:24:15 -0500

    completed Pokedex Search

---
+++ src/app/pokedex/PokemonCard.tsx
@@ -0,0 +1,69 @@
+// src/components/PokemonCard.tsx
+// src/components/PokemonCard.tsx
+import { Box, Heading, Image, Text } from "@/components/Chakra";
+import React from "react";
+
+interface PokemonCardProps {
+  id: number;
+  name: string;
+  type: string;
+  imageUrl?: string;
+}
+const formatId = (id: number) => id.toString().padStart(3, "0");
+
+const PokemonCard: React.FC<PokemonCardProps> = ({
+  id,
+  name,
+  type,
+  imageUrl,
+}) => (
+  <Box
+    p="4" // Increase padding to make the card larger
+    borderRadius="lg"
+    border="2px solid #9AA4D3"
+    width="100%"
+    bg="#DDEFF5"
+    textAlign="left"
+  >
+    {/* Green box with the Pokemon image */}
+    <Box
+      bg="gray.200"
+      p="2"
+      borderRadius="md"
+      mb="4"
+      textAlign="center"
+      maxH="100px"
+    >
+      <Image src={imageUrl} alt={name} maxH="80px" maxW="80px" mx="auto" />
+    </Box>
+
+    {/* ID and Type */}
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      mb="2"
+      textAlign="center"
+      mt="2"
+    >
+      <Text fontWeight="bold" color="gray.700">
+        #{formatId(id)}
+      </Text>
+      <Text
+        fontWeight="bold"
+        color="gray.700"
+        bg="gray.400"
+        px="2"
+        borderRadius="md"
+      >
+        {type}
+      </Text>
+    </Box>
+
+    {/* Pokemon Name */}
+    <Heading as="h2" fontSize="xl" mb="2" color="gray.700">
+      {name}
+    </Heading>
+  </Box>
+);
+
+export default PokemonCard;
---
+++ src/app/pokedex/page.tsx
@@ -0,0 +1,65 @@
+// src/app/pokedex/page.tsx
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@/components/Chakra";
+import Navbar from "@/components/Navbar";
+import React from "react";
+import PokemonCard from "./PokemonCard";
+
+// Sample data for Bulbasaur and Pikachu
+const pokemonData = [
+  { id: 1, name: "Bulbasaur", type: "Grass", imageUrl: "bulbasaur.png" },
+  { id: 25, name: "Pikachu", type: "Electric", imageUrl: "pikachu.png" },
+  // Add more Pokemon data as needed
+];
+
+const Pokedex: React.FC = () => (
+  <>
+    <Box maxW="4xl" mx="auto" mt={20} textAlign="center">
+      <Heading size="3xl" mb="2">
+        Pokédex
+      </Heading>
+      <Text as="span" color="gray.600" fontWeight="bold" mb="8">
+        Search for a Pokemon in the database
+      </Text>
+
+      {/* Search Box and Go Button */}
+      <Flex align="center" justify="center" mb="8" mt="8">
+        <Input
+          type="text"
+          placeholder="🔎 Enter Pokemon Name"
+          mr="2"
+          bg="#FFA7A7" // Set background color
+          border="2px solid #FE4040" // Set border color
+          fontWeight="bold"
+        />
+        <Button
+          colorScheme="teal"
+          bg="#FE4040" // Set background color
+        >
+          GO
+        </Button>
+      </Flex>
+
+      {/* Divider */}
+      <Divider borderColor="#FF7171" borderWidth="2px" mb="4" mt={4} />
+
+      {/* Pokemon Map */}
+      <SimpleGrid columns={{ base: 2, md: 3, lg: 4 }} spacing={6}>
+        {pokemonData.map((pokemon) => (
+          <PokemonCard key={pokemon.id} {...pokemon} />
+        ))}
+      </SimpleGrid>
+    </Box>
+  </>
+);
+
+export default Pokedex;
