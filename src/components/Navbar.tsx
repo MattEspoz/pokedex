95fbab6d9490ac8711d682120d488da88147cd6b
Matt Espinoza <70907264+MattEspoz@users.noreply.github.com>
2023-11-20 13:56:18 -0500

    navbar is functional

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,13 @@
 import {
   Box,
   Button,
+  Link as ChakraLink,
   Container,
   Flex,
   Image,
-  Link,
   Text,
 } from "@chakra-ui/react";
+import Link from "next/link";
 
 const Navbar = () => (
   <Box
@@ -22,61 +23,69 @@ const Navbar = () => (
     <Container maxW="4xl">
       <Flex justify="space-between" align="center">
         {/* Pokédex Button */}
-
-        <Button
-          bg="#030201"
-          color="#FFFFFF"
-          variant="outline"
-          borderColor="#FFFFFF"
-          _hover={{ bg: "#1a1a1a" }}
-          border="2px solid #FFFFFF"
-        >
-          <Image src="/pokedex.png" alt="Play Game" maxW="20px" maxH="20px" />
-          <Text ml="2">Pokedex</Text>
-        </Button>
+        <Link href="/pokedex">
+          <ChakraLink>
+            <Button
+              bg="#030201"
+              color="#FFFFFF"
+              variant="outline"
+              borderColor="#FFFFFF"
+              _hover={{ bg: "#1a1a1a" }}
+              border="2px solid #FFFFFF"
+            >
+              <Image src="/pokedex.png" alt="Pokedex" maxW="20px" maxH="20px" />
+              <Text ml="2">Pokedex</Text>
+            </Button>
+          </ChakraLink>
+        </Link>
 
         {/* Empty Box for spacing */}
         <Box flex="1" />
         <Flex align="center" gap="4">
           {/* Play Game Button */}
-          <Button
-            bg="#030201"
-            color="#FFFFFF"
-            variant="outline"
-            borderColor="#FFFFFF"
-            _hover={{ bg: "#1a1a1a" }}
-            border="2px solid #FFFFFF"
-          >
-            <Image
-              src="/pokeball.png"
-              alt="Play Game"
-              maxW="20px"
-              maxH="20px"
-            />
-            <Text ml="2">Play Game</Text>
-          </Button>
+          <Link href="/game">
+            <ChakraLink>
+              <Button
+                bg="#030201"
+                color="#FFFFFF"
+                variant="outline"
+                borderColor="#FFFFFF"
+                _hover={{ bg: "#1a1a1a" }}
+                border="2px solid #FFFFFF"
+              >
+                <Image
+                  src="/pokeball.png"
+                  alt="Play Game"
+                  maxW="20px"
+                  maxH="20px"
+                />
+                <Text ml="2">Play Game</Text>
+              </Button>
+            </ChakraLink>
+          </Link>
 
           {/* Profile Button */}
-
-          <Link>
-            <Button
-              bg="#A2DAF2"
-              color="#FFFFFF"
-              variant="outline"
-              borderColor="#274288"
-              _hover={{ bg: "#1a1a1a" }}
-              border="2px solid #274288"
-            >
-              <Image
-                src="/character.png"
-                alt="Play Game"
-                maxW="20px"
-                maxH="20px"
-              />
-              <Text ml="2" color="black">
-                Profile
-              </Text>
-            </Button>
+          <Link href="/profile">
+            <ChakraLink>
+              <Button
+                bg="#A2DAF2"
+                color="#FFFFFF"
+                variant="outline"
+                borderColor="#274288"
+                _hover={{ bg: "#1a1a1a" }}
+                border="2px solid #274288"
+              >
+                <Image
+                  src="/character.png"
+                  alt="Profile"
+                  maxW="20px"
+                  maxH="20px"
+                />
+                <Text ml="2" color="black">
+                  Profile
+                </Text>
+              </Button>
+            </ChakraLink>
           </Link>
         </Flex>
       </Flex>
