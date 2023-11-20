87cdc6f3c0048f8e341dd4db0b38975304dd0cf8
Matt Espinoza <70907264+MattEspoz@users.noreply.github.com>
2023-11-20 14:43:02 -0500

    completed my profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -8,12 +8,12 @@ const Profile = () => {
   // console.log(supabase);
 
   const [pokemon, setPokemon] = useState<null | any[]>(null);
+  const [pokemonDetails, setPokemonDetails] = useState<null | any[]>(null);
 
   useEffect(() => {
     const fetchPokemon = async () => {
-      const { data, error } = await supabase.from("Gym").select();
+      const { data, error } = await supabase.from("Trainer").select();
       if (error) console.log(error);
-      console.log(data);
 
       if (data) {
         setPokemon(data);
@@ -21,6 +21,42 @@ const Profile = () => {
     };
 
     fetchPokemon();
+  }, []);
+
+  const fetchAllPokemonIds = async () => {
+    const { data, error } = await supabase.from("Pokemon").select("pokemon_id");
+
+    if (error) {
+      console.error(error);
+      return [];
+    }
+
+    if (data) {
+      return data.map((pokemon) => pokemon.pokemon_id);
+    }
+
+    return [];
+  };
+
+  useEffect(() => {
+    const fetchPokemonDetails = async () => {
+      const allPokemonIds = await fetchAllPokemonIds();
+
+      if (allPokemonIds.length > 0) {
+        const { data, error } = await supabase
+          .from("Pokemon")
+          .select()
+          .in("pokemon_id", allPokemonIds);
+
+        if (error) console.error(error);
+
+        if (data) {
+          setPokemonDetails(data);
+        }
+      }
+    };
+
+    fetchPokemonDetails();
   }, []);
 
   return (
@@ -31,7 +67,7 @@ const Profile = () => {
         </Heading>
         {pokemon && (
           <Text as="span" color="gray.600" fontWeight="bold" ml={4}>
-            You have collected {pokemon.length} Pokemon.
+            View the Pokemon you have collected
           </Text>
         )}
         <Divider borderColor="#FF7171" borderWidth="2px" mb="4" mt={4} ml={4} />
@@ -61,10 +97,41 @@ const Profile = () => {
           {/* Right Column */}
           <Box width={{ base: "100%", md: "75%" }}>
             {/* Title for Pokemon Collected */}
-            <Heading color="#544E4E" fontSize="4xl" mb="2" mt="4">
+            <Heading color="#544E4E" fontSize="4xl" mt="4">
               Pokemon Collected
             </Heading>
+            {pokemon && (
+              <Text as="span" color="gray.600" fontWeight="bold">
+                You have captured{" "}
+                <Box as="span" color="#AA3030" textDecoration="underline">
+                  {pokemon[0]?.pokemon_ids.length}
+                </Box>{" "}
+                Pokemon.
+              </Text>
+            )}
             {/* TODO: Map and display the collected Pokemon */}
+            {/* Map and display the collected Pokemon */}
+            <Flex flexWrap="wrap">
+              {pokemon &&
+                pokemonDetails &&
+                pokemon[0]?.pokemon_ids.map((id: number, index: number) => (
+                  <Box
+                    key={id}
+                    bg="#DDEFF5"
+                    p="4"
+                    borderRadius="md"
+                    border="2px solid #9AA4D3"
+                    mt="4"
+                    mr="4"
+                  >
+                    {/* Display Pokemon data here */}
+                    <Text color="black" fontWeight="bold" textAlign="center">
+                      {pokemonDetails[index]?.name || `Pokemon #${id}`}
+                    </Text>
+                    {/* Additional Pokemon data can be added as needed */}
+                  </Box>
+                ))}
+            </Flex>
           </Box>
         </Flex>
       </Box>
